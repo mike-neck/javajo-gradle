b39f1beea20a66168df3d4550c1fd323eefa59c7
mike_neck <jkrt3333@gmail.com>
2015-09-18 00:02:34 +0900

    add all tests

--- java-projects/fail-tests/script/generate-test-files.groovy
+++ java-projects/fail-tests/script/generate-test-files.groovy
@@ -19,12 +19,72 @@ import java.nio.file.*
 def self = Paths.get('build.gradle').toAbsolutePath()
 def root = self.parent
 
-if (root.fileName.endsWith('javajo-gradle')) {
-    throw new IllegalStateException('Please execute this script at root directory. [javajo-gradle]')
+if (!root.endsWith('javajo-gradle')) {
+    throw new IllegalStateException("Please execute this script at root directory. expected: [javajo-gradle], current: [${root.fileName}]")
 }
 
 def project = root.resolve('java-projects/fail-tests')
 def srcDir = project.resolve('src/test/java')
 def rootPackage = 'javajo.sample.tests'
 
+def names = [
+        'sheepdog', 'foxhound', 'bulldog', 'chihuahua', 'dalmatian'
+]
 
+def asTypeName = {String name ->
+    "${name.substring(0,1).toUpperCase()}${name.substring(1)}"
+}
+
+names.each {pkg ->
+    def packageName = "$rootPackage.$pkg"
+    def dir = srcDir.resolve(packageName.replace('.','/'))
+    names.each {name ->
+        def type = "${asTypeName(name)}Test"
+        def classContents = """
+package ${packageName};
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import javajo.sample.TestTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ${type} {
+
+    @Rule
+    public final TestTime testTime = new TestTime();
+
+    @After
+    public void teardown() {
+        System.out.println(testTime.getStamp());
+    }
+
+    @Test
+    public void testName() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getTestName())
+                .isEqualTo("testName");
+    }
+
+    @Test
+    public void className() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getClassName())
+                .isEqualTo("${packageName}.${type}");
+    }
+
+    @Test
+    public void stamp() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getStamp())
+                .contains("${packageName}.${type}#stamp");
+    }
+}
+"""
+        if (!Files.exists(dir)) {
+            Files.createDirectories(dir)
+        }
+        dir.resolve("${type}.java").toFile().write(classContents, 'UTF-8')
+    }
+}
---
+++ java-projects/fail-tests/src/test/java/javajo/sample/tests/chihuahua/ChihuahuaTest.java
@@ -0,0 +1,41 @@
+
+package javajo.sample.tests.chihuahua;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import javajo.sample.TestTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChihuahuaTest {
+
+    @Rule
+    public final TestTime testTime = new TestTime();
+
+    @After
+    public void teardown() {
+        System.out.println(testTime.getStamp());
+    }
+
+    @Test
+    public void testName() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getTestName())
+                .isEqualTo("testName");
+    }
+
+    @Test
+    public void className() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getClassName())
+                .isEqualTo("javajo.sample.tests.chihuahua.ChihuahuaTest");
+    }
+
+    @Test
+    public void stamp() throws InterruptedException {
+        Thread.sleep(1200l);
+        assertThat(testTime.getStamp())
+                .contains("javajo.sample.tests.chihuahua.ChihuahuaTest#stamp");
+    }
+}
